1a644f10612e5f4577bc7eb2405e14a032f14b5d
Yaosquared <marioyaoyao14@gmail.com>
2022-12-08 19:58:52 +0800

    first commit

---
+++ easy-edit/pages/services.js
@@ -0,0 +1,11 @@
+import Services from "../components/Services"
+import FooterServices from "../components/FooterServices"
+
+export default function EEServices() {
+    return (
+        <div>
+            <Services />
+            <FooterServices />
+        </div>
+    );
+}
